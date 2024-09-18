762a319d0a692e9552615ec0cc03a6579c0869c8
unknown <antonionelsonsof@gmail.com>
2024-09-17 21:16:16 -0300

    time created corrected

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,12 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('reservacuca', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  
+  dialectOptions: {
+    timezone: 'Etc/GMT0',
+  },
+  timezone: '-03:00'
 });
 
 module.exports = sequelize;
